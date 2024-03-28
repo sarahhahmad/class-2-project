f27f92d9ffb7083846581808ab022354e97233ba
Sarah Ahmad <saa804@nyu.edu>
2024-03-28 19:34:45 -0400

    port jsfiddle into html, css, and js files

---
+++ scripts.js
@@ -0,0 +1,17 @@
+$('.card').on('click', function () {
+    var artist = $(this).data('artist')
+
+    $('#column-right').empty()
+
+    if (artist === 'nirvana') {
+        $('#column-right').css('background-image', 'url("https://people.com/thmb/FM0c0-njIY8t6hF8V4YmbW8b-o0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(749x0:751x2)/nirvana-660-0269944e1daa401199afb0b9b4a8706e.jpg")')
+    }
+
+    if (artist === 'blink-182') {
+        $('#column-right').css('background-image', 'url("https://upload.wikimedia.org/wikipedia/en/2/21/Blink-182_-_Dude_Ranch_cover.jpg")')
+    }
+
+    if (artist === 'radiohead') {
+        $('#column-right').css('background-image', 'url("https://upload.wikimedia.org/wikipedia/en/b/ba/Radioheadokcomputer.png")')
+    }
+})
